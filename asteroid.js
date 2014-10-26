d2b459ac6dc90a5a2c604375ad50af4c82a20415
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-10-26 18:51:10 +0100

    Overwritten origin checking function

--- asteroid.js
+++ asteroid.js
@@ -14,7 +14,16 @@ OAuth._endOfPopupResponseTemplate = Assets.getText("end_of_popup_response.html")
 	.replace("##ALLOWED_ORIGIN##", OAuth.allowedOrigin)
 	.replace("##SCRIPT##", Assets.getText("end_of_popup_response.js"));
 
-// Overwrite the redirect template
-OAuth._endOfRedirectResponseTemplate = Assets.getText("end_of_redirect_response.html")
-	.replace("##ALLOWED_ORIGIN##", OAuth.allowedOrigin)
-	.replace("##SCRIPT##", Assets.getText("end_of_redirect_response.js"));
+// Overwrite the redirect origin checking function to allow
+// our alternative origin
+OAuth._checkRedirectUrlOrigin = function (redirectUrl) {
+	var appHost = Meteor.absoluteUrl();
+	var appHostReplacedLocalhost = Meteor.absoluteUrl(undefined, {
+		replaceLocalhost: true
+	});
+	return (
+		redirectUrl.substr(0, appHost.length) !== appHost &&
+		redirectUrl.substr(0, appHostReplacedLocalhost.length) !== appHostReplacedLocalhost &&
+		redirectUrl.substr(0, OAuth.allowedOrigin.length) !== OAuth.allowedOrigin
+	);
+};
